cd6616d9ff35c8e5ca225e7dcad9dc1f32360a59
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 21:30:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,36 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  overall_lowest_price = 0
+  overall_lowest_person = ""
+  
+  last_price = 0
+  last_name = ""
+  if name_hash != {}
+    name_hash.each do |name, price|
+      current_price = price
+      current_name = name
+    
+      if current_price < last_price 
+        overall_lowest_person = name_hash[current_name]
+        overall_lowest_price = current_price
+      
+      elsif current_price > last_price
+        overall_lowest_person = name_hash[last_name]
+        overall_lowest_price = last_price
+      
+      elsif last_price == ""
+        last_price = current_price
+        last_name = current_name
+        overall_lowest_person = name_hash[current_name]
+        overall_lowest_price = current_price
+      else
+        nil
+      end
+    overall_lowest_person
+    end
+  else
+     nil
+  end
 end
